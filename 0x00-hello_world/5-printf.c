907e63a26d7bc8223373da74d49a7fc5bc1f5da5
root <root@LAPTOP-SI2L0VM7>
2024-04-25 20:50:50 +0200

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - ' entry point'
+ *
+ * Description: 'using printf to output str'
+ *
+ * Return: always 0 (successful)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
