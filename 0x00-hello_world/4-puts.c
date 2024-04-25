06c2c01ea467549ef0e4e1fc5cc942c1396f146f
root <root@LAPTOP-SI2L0VM7>
2024-04-25 19:54:08 +0200

    fixing 4-puts.c betty-style coding error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main: this is the entry point of the programm
+ * returns a string "Programming is like building a multilingual puzzle"
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
-	return(0);
-
+	return (0);
 }
